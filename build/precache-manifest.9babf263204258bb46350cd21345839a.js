f95bb8d5c05ada4c2337b32c925ac6a4d019db28
Nick <shuminskiy.nik@mail.ru>
2021-09-05 00:47:16 +0300

    Server error handling

---
+++ build/precache-manifest.9babf263204258bb46350cd21345839a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39850aa2c9804dd78cc7f69cb0333228",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "806d73149341ad12c0d6",
+    "url": "/TodoList/static/css/main.2017d655.chunk.css"
+  },
+  {
+    "revision": "f13f20ffe486c12acabd",
+    "url": "/TodoList/static/js/2.4e2bfe84.chunk.js"
+  },
+  {
+    "revision": "936711c3f4be0c9c88207c43cda44117",
+    "url": "/TodoList/static/js/2.4e2bfe84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "806d73149341ad12c0d6",
+    "url": "/TodoList/static/js/main.573cae7f.chunk.js"
+  },
+  {
+    "revision": "cec92663b5aa42fa8dea",
+    "url": "/TodoList/static/js/runtime-main.46cf6363.js"
+  }
+]);
